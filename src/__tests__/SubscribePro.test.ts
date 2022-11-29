f720da8addc645948a93dcbcb121de675a0e3bc1
Ryan Garver <ragarver@gmail.com>
2022-11-29 11:11:54 -0800

    chore: Add initial testing configuration and automation

---
+++ src/__tests__/SubscribePro.test.ts
@@ -0,0 +1,22 @@
+import { SubscribePro } from '../';
+
+test('SubscribePro is defined', () => {
+  expect(SubscribePro).toBeDefined();
+});
+
+test('SubscribePro.config is defined', () => {
+  expect(SubscribePro.config).toBeDefined();
+});
+
+test('SubscribePro.config.endpointUrl has good default', () => {
+  expect(SubscribePro.config.endpointUrl).toBe('https://api.subscribepro.com');
+});
+
+test('config can be set', () => {
+  let [accessToken, environmentKey] = ['ACCESS_TOKEN', 'ENVIRONMENT_KEY'];
+  SubscribePro.configure({
+    accessToken,
+    environmentKey
+  });
+  expect(SubscribePro.config.accessToken).toBe(accessToken);
+});
