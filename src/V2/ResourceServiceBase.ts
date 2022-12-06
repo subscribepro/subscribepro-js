bfcae896c75e38fd6db03c6e07f883d61152e0c8
Ryan Garver <ryan.garver@subscribepro.com>
2022-12-06 11:10:27 -0800

    feat(v2-api): Add V2 Customers endpoints (#8)
    bug(v2-api): Parameters on create and update actions on V2 endpoints are nested under
    the resource name and needed to be supported.
    chore(api): Refactor the ResourceServiceBase in to a mixin based system
    for both V2 and V3 to support different capabilities of different
    resources while exporting a well typed interface.

--- src/V2/ResourceServiceBase.ts
+++ src/V2/ResourceServiceBase.ts
@@ -1,6 +1,105 @@
 import Client, { JSONObject, JSONValue } from '../Client';
 import SubscribePro from '../SubscribePro';
 
+interface ResourceService<RecordType, CollectionType> {
+  resourceName(): string;
+  collectionName(): string;
+  collectionPath():string;
+  singularPath():string;
+  resourcePath({id}:{id:string|number}):string;
+
+  processJSONToRecord(json: JSONObject | null): RecordType | null;
+  processJSONToCollection(json: JSONObject | null): CollectionType | null;
+};
+
+export abstract class ResourceServiceBase<RecordType, CollectionType> implements ResourceService<RecordType, CollectionType> {
+  abstract resourceName(): string;
+  abstract collectionName(): string;
+  abstract collectionPath(): string;
+  abstract singularPath(): string;
+  abstract resourcePath({id}:{id:string|number}): string;
+
+  processJSONToRecord(json: JSONObject | null): RecordType | null {
+    return json ? json as RecordType : null;
+  }
+
+  processJSONToCollection(json: JSONObject | null): CollectionType | null {
+    if (json) {
+      return json[this.collectionName()] as CollectionType;
+    } else {
+      return null;
+    }
+  }
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ResourceServiceBaseConstructor<TResult> = new (...args: any[]) => TResult;
+
+export function ResourceCRUable<
+  T extends ResourceServiceBaseConstructor<ResourceService<RecordType, CollectionType>>,
+  RecordType, CollectionType=RecordType[],
+  CreateType=Partial<RecordType>, UpdateType=CreateType
+>(Base: T) {
+  return class extends Base {
+    async getOne({ client, id }: { client?: Client, id: string|number }): Promise<RecordType | null> {
+      client ||= SubscribePro.client;
+
+      const response = await client.request({ path: this.resourcePath({id}), method: "GET" });
+      return this.processJSONToRecord(response);
+    }
+
+    async getAll({ client }: { client?: Client }): Promise<CollectionType | null> {
+      client ||= SubscribePro.client;
+
+      const response = await client.request({ path: this.collectionPath(), method: "GET"});
+      return this.processJSONToCollection(response);
+    }
+
+    async createOne({ client, data }: { client?: Client, data: CreateType }): Promise<RecordType | null> {
+      client ||= SubscribePro.client;
+
+      const response = await client.request({
+        path: this.singularPath(),
+        method: "POST",
+        body: JSON.stringify({[this.resourceName()]: data}),
+      });
+      return this.processJSONToRecord(response);
+    }
+
+    async updateOne({ client, id, data }: { client?: Client, id: string|number, data: UpdateType }): Promise<RecordType | null> {
+      client ||= SubscribePro.client;
+
+      const response = await client.request({
+        path: this.resourcePath({id}),
+        method: "POST",
+        body: JSON.stringify({[this.resourceName()]: data}),
+      });
+      return this.processJSONToRecord(response);
+    }
+
+    
+  };
+};
+
+export function ResourceBulkCreateable<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  T extends ResourceServiceBaseConstructor<ResourceService<any, any>>,
+  BulkCreateType
+>(Base: T) {
+  return class extends Base {
+    async createAll({ client, data }: { client?: Client, data: BulkCreateType }): Promise<null> {
+      client ||= SubscribePro.client;
+  
+      await client.request({
+        path: this.collectionPath(),
+        method: "POST",
+        body: JSON.stringify({[this.collectionName()]: data}),
+      });
+      return null;
+    }
+  }
+};
+
 export type JSONPatchData = {
   op: "add" | "remove" | "replace" | "move" | "copy" | "test";
   ids?: string[];
@@ -8,94 +107,33 @@ export type JSONPatchData = {
   value?: JSONValue;
 };
 
-export abstract class ResourceServiceBase<
-  RecordType,
-  CreateType=RecordType,
-  BulkCreateType=CreateType[],
-  UpdateType=CreateType,
-  PatchType=JSONPatchData,
-  BulkPatchType=PatchType[],
-  CollectionType=RecordType[]
-> {
-  abstract collectionPath():string;
-  abstract singularPath():string;
-  abstract resourcePath({id}:{id:string|number}):string;
+export function ResourcePatchable<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  T extends ResourceServiceBaseConstructor<ResourceService<RecordType, any>>,
+  RecordType
+>(Base: T) {
+  return class extends Base {
+    async patchOne({ client, id, data }: { client?: Client, id: string|number, data: JSONPatchData }): Promise<RecordType | null> {
+      client ||= SubscribePro.client;
 
-  processJSONToRecord(json: JSONObject | null): RecordType | null {
-    return json ? json as RecordType : null;
-  }
+      const response = await client.request({
+        path: this.resourcePath({id}),
+        method: "PATCH",
+        body: JSON.stringify(data),
+      });
+      return this.processJSONToRecord(response);
+    }
 
-  processJSONToCollection(json: JSONObject | null): CollectionType | null {
-    return json ? json as CollectionType : null;
-  }
+    async patchAll({ client, data }: { client?: Client, data: JSONPatchData[] }): Promise<null> {
+      client ||= SubscribePro.client;
 
-  async getOne({ client, id }: { client?: Client, id: string|number }): Promise<RecordType | null> {
-    client ||= SubscribePro.client;
-
-    const response = await client.request({ path: this.resourcePath({id}), method: "GET" });
-    return this.processJSONToRecord(response);
-  }
-
-  async getAll({ client }: { client?: Client }): Promise<CollectionType | null> {
-    client ||= SubscribePro.client;
-
-    const response = await client.request({ path: this.collectionPath(), method: "GET"});
-    return this.processJSONToCollection(response);
-  }
-
-  async createOne({ client, data }: { client?: Client, data: CreateType }): Promise<RecordType | null> {
-    client ||= SubscribePro.client;
-
-    const response = await client.request({
-      path: this.singularPath(),
-      method: "POST",
-      body: JSON.stringify(data),
-    });
-    return this.processJSONToRecord(response);
-  }
-
-  async createAll({ client, data }: { client?: Client, data: BulkCreateType }): Promise<null> {
-    client ||= SubscribePro.client;
-
-    await client.request({
-      path: this.collectionPath(),
-      method: "POST",
-      body: JSON.stringify(data),
-    });
-    return null;
-  }
-
-  async updateOne({ client, id, data }: { client?: Client, id: string|number, data: UpdateType }): Promise<RecordType | null> {
-    client ||= SubscribePro.client;
-
-    const response = await client.request({
-      path: this.resourcePath({id}),
-      method: "POST",
-      body: JSON.stringify(data),
-    });
-    return this.processJSONToRecord(response);
-  }
-
-  async patchOne({ client, id, data }: { client?: Client, id: string|number, data: PatchType }): Promise<RecordType | null> {
-    client ||= SubscribePro.client;
-
-    const response = await client.request({
-      path: this.resourcePath({id}),
-      method: "PATCH",
-      body: JSON.stringify(data),
-    });
-    return this.processJSONToRecord(response);
-  }
-
-  async patchAll({ client, data }: { client?: Client, data: BulkPatchType }): Promise<null> {
-    client ||= SubscribePro.client;
-
-    await client.request({
-      path: this.collectionPath(),
-      method: "PATCH",
-      body: JSON.stringify(data),
-    });
-    return null;
+      await client.request({
+        path: this.collectionPath(),
+        method: "PATCH",
+        body: JSON.stringify(data),
+      });
+      return null;
+    }
   }
 };
 
--- src/V2/index.ts
+++ src/V2/index.ts
@@ -1,7 +1,9 @@
 import Products from "./Products";
+import Customers from "./Customers";
 
 export const V2 = {
   Products,
+  Customers,
 };
 
 export default V2;
